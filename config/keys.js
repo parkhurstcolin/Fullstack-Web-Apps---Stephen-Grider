39780d61e3e30b67735ed68860d5303f44194ced
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-09-01 09:44:11 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   .gitignore
    	new file:   config/keys.js

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+//keys.js - figure out what set of credentials to return
+if (process.env.NODE_ENV === "production") {
+	// we are in production - return the prod set of keys
+} else {
+	// we are in development - return the dev set of keys
+}
