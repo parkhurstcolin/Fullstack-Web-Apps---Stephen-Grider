f2174ae932c94f11f24a98fd189098990f0e113c
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-09-05 20:46:21 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   ssl-index.js

--- ssl-index.js
+++ ssl-index.js
@@ -31,9 +31,9 @@ require("./routes/authRoutes")(app);
 //Declare location for server certificates
 if (process.env.NODE_ENV === "production") {
 	const httpsOptions = {
-		cert: keys.cert,
-		ca: keys.ca,
-		key: keys.key,
+		cert: fs.readFileSync(keys.cert),
+		ca: fs.readFileSync(keys.ca),
+		key: fs.readFileSync(keys.key),
 	};
 	const hostname = "emaily.xyz";
 
