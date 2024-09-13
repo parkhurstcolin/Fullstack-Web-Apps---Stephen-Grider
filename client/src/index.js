204fdd65fa511a43b256506f546791ea328ac2de
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-09-13 10:31:20 -0400

    71

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,15 @@
+//import React & ReactDOM library
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDom from "react-dom/client";
 
-ReactDOM.render();
+//Import App component as usual
+import App from "./Components/App";
+
+//Get a reference to the div with ID root
+const el = document.getElementById("root");
+
+//Tell React to take control of that element
+const root = ReactDom.createRoot(el);
+
+//Render the component
+root.render(<App />);
