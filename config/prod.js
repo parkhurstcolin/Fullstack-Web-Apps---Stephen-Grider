cd0c7b5e784bd7753c9848597de183e2cd244164
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-09-19 11:03:26 -0400

    103

--- config/prod.js
+++ config/prod.js
@@ -1,11 +1,18 @@
 module.exports = {
+	//NodeJS Keys
 	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	googleRedirectURI: process.env.GOOGLE_REDIRECT_URI,
 	mongoURI: process.env.MONGO_URI,
 	cookieKey: process.env.COOKIE_KEY,
+
+	//Server Keys
 	cert: process.env.WEB_CERT,
 	ca: process.env.CA_CERT,
 	key: process.env.WEB_KEY,
+
+	//REACT Keys
+	stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+	stripeSecretKey: process.env.STRIPE_SECRET_KEY,
 };
 
