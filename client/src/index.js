51298ab03c5ee7e7f42764ecefffc5c45b838afd
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-09-20 11:00:36 -0400

    104

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import "materialize-css/dist/css/materialize.min.css";
 
 //import React & ReactDOM library
 import React from "react";
-import ReactDom from "react-dom/client";
+import ReactDOM from "react-dom";
 
 //import React-Redux & createStore
 import { Provider } from "react-redux";
@@ -14,19 +14,17 @@ import { thunk } from "redux-thunk";
 
 //Import App component as usual
 import App from "./components/App";
-
 import reducers from "./reducers";
+
 const store = createStore(reducers, {}, applyMiddleware(thunk));
 
-//Get a reference to the div with ID root
-const el = document.getElementById("root");
-
-//Tell React to take control of that element
-const root = ReactDom.createRoot(el);
-
 //Render the component
-root.render(
+ReactDOM.render(
 	<Provider store={store}>
 		<App />
-	</Provider>
+	</Provider>,
+	document.querySelector("#root")
 );
+
+console.log("STRIPE KEY IS", process.env.REACT_APP_STRIPE_KEY);
+console.log("Environment is ", process.env.NODE_ENV);
