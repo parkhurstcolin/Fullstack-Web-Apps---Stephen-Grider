28417fef82977ee3fc230a1f95d06931cfdebe48
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-10-03 10:52:56 -0400

    Build 2

--- ssl-index.js
+++ ssl-index.js
@@ -54,13 +54,12 @@ if (process.env.NODE_ENV === "production") {
 		next();
 	});
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-	});
-
 	//Listening on ports 80 & 443
 	httpServer.listen(80, hostname);
 	httpsServer.listen(443, hostname);
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
 } else {
 	//Dev environment
 	const PORT = process.env.PORT || 5000;
