0a11fe88796b5e4c7c5bdbf803c35366c77eb603
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-10-05 10:21:01 -0400

    undo

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,11 +2,11 @@ import axios from "axios";
 import { FETCH_USER } from "./types";
 
 export const fetchUser = () => async (dispatch) => {
-	const res = await axios.get("http://localhost:5000/api/current_user");
+	const res = await axios.get("/api/current_user");
 	dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const handleToken = (token) => async (dispatch) => {
-	const res = await axios.post("http://localhost:5000/api/stripe", token);
+	const res = await axios.post("/api/stripe", token);
 	dispatch({ type: FETCH_USER, payload: res.data });
 };
