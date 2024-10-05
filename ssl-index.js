dc3474a8e226886a5de57a24515add7eac8594eb
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-10-04 21:44:54 -0400

    Still testing

--- ssl-index.js
+++ ssl-index.js
@@ -46,7 +46,7 @@ if (process.env.NODE_ENV === "production") {
 
 	//Create server for http
 	const httpServer = http.createServer(app);
-	const httpsServer = https.createServer(httpsOptions, app);
+	//const httpsServer = https.createServer(httpsOptions, app);
 
 	//Redirect from http to https
 	//	app.set((req, res, next) => {
@@ -54,15 +54,15 @@ if (process.env.NODE_ENV === "production") {
 	//	});
 
 	app.get("*", (req, res) => {
+		//if (req.protocol === "http") {
+		//	res.redirect(301, `https://${req.headers.host}${req.url}`);
+		//}
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-		if (req.protocol === "http") {
-			res.redirect(301, `https://${req.headers.host}${req.url}`);
-		}
 	});
 
 	//Listening on ports 80 & 443
 	httpServer.listen(80, hostname);
-	httpsServer.listen(443, hostname);
+	//httpsServer.listen(443, hostname);
 }
 	//Dev environment
 	const PORT = process.env.PORT || 5000;
