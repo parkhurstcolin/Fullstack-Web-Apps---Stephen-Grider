a68bd3da91e01ec84bb18728c504636a4a193044
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-10-06 01:25:01 -0400

    f

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,14 +3,14 @@ import { FETCH_USER } from "./types";
 
 export const fetchUser = () => async (dispatch) => {
 	const res = await axios.get(
-		`https://${process.env.REACT_APP_ROUTE}/api/current_user`
+		`http://${process.env.REACT_APP_ROUTE}/api/current_user`
 	);
 	dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const handleToken = (token) => async (dispatch) => {
 	const res = await axios.post(
-		`https://${process.env.REACT_APP_ROUTE}/api/stripe`,
+		`http://${process.env.REACT_APP_ROUTE}/api/stripe`,
 		token
 	);
 	dispatch({ type: FETCH_USER, payload: res.data });
