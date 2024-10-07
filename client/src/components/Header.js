a878023933b8378cc53950e6785e2eb43347a5e7
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-10-06 21:24:48 -0400

    test

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -12,7 +12,7 @@ class Header extends Component {
 			case false:
 				return (
 					<li>
-						<a href={`/auth/google`}>Login with Google!</a>
+						<a href="/auth/google">Login with Google!</a>
 					</li>
 				);
 			default:
@@ -24,7 +24,7 @@ class Header extends Component {
 						Credits: {this.props.auth.credits}
 					</li>,
 					<li key="3">
-						<a href={`/api/logout`}>Logout</a>
+						<a href="/api/logout">Logout</a>
 					</li>,
 				];
 		}
