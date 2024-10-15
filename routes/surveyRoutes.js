5a3f080f3be0d5a4fcbdf5c4537ee5604455f0cf
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-10-15 14:40:37 -0400

    135

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,11 @@
 const requireLogin = require("../middlewares/requireLogin");
+const requireCredits = require("../middlewares/requireCredits");
 
 module.exports = (app) => {
-	app.post("/api/surveys", requireLogin, async (req, res) => {});
+	app.post(
+		"/api/surveys",
+		requireLogin,
+		requireCredits,
+		async (req, res) => {}
+	);
 };
