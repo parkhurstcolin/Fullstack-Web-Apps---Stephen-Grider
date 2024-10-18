1a11e7899f1a7e8cc660accae5b32d2c6a48233e
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-10-18 11:06:32 -0400

    145

--- services/Mailer.js
+++ services/Mailer.js
@@ -2,4 +2,8 @@ const sendgrid = require("sendgrid");
 const helper = sendgrid.mail;
 const keys = require("../config/keys");
 
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+	//Done
+}
+
+module.exports = Mailer;
