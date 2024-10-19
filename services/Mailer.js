e39b761f15681974f6588e63d4596e8ba43ce99a
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-10-19 11:08:27 -0400

    148

--- services/Mailer.js
+++ services/Mailer.js
@@ -11,9 +11,9 @@ class Mailer extends helper.Mail {
 		this.body = new helper.Content("text/html", content);
 		this.recipients = this.formatAddresses(recipients);
 
-    this.addContent(this.body);
-    this.addClickTracking();
-    this.addRecipients();
+		this.addContent(this.body);
+		this.addClickTracking();
+		this.addRecipients();
 	}
 
 	formatAddresses(recipients) {
@@ -22,12 +22,21 @@ class Mailer extends helper.Mail {
 		});
 	}
 
-  addClickTracking(){
+	addClickTracking() {
 		const trackingSettings = new helper.TrackingSettings();
 		const clickTracking = new helper.ClickTracking(true, true);
 
 		trackingSettings.setClickTracking(clickTracking);
 		this.addTrackingSettings(trackingSettings);
+	}
+
+	addRecipients() {
+		const personalize = new helper.Personalization();
+
+		this.recipients.forEach((recipient) => {
+			personalize.addTo(recipient);
+		});
+		this.addPersonalization(personalize);
 	}
 }
 
