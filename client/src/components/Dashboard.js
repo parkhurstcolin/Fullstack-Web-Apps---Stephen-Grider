4ec473e233de380462e9fb7c32e5ccf761cdddfa
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-10-26 10:34:00 -0400

    156

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Dashboard = () => {
-	return <div>Dashboard</div>;
+	return (
+		<>
+			<div>Dashboard</div>
+			<div className="fixed-action-btn">
+				<a className="btn-floating btn-large red">
+					<i className="large material-icons">add</i>
+				</a>
+			</div>
+		</>
+	);
 };
 
 export default Dashboard;
