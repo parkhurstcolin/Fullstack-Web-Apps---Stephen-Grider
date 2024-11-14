511b74ed26642a00dbcf741b7e9f0874bec4f1c6
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-11-14 11:06:30 -0500

    175

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import SurveyForm from "./SurveyForm";
+import SurveyFormReview from "./SurveyFormReview";
 
 class SurveyNew extends Component {
-	render() {
+	state = { showFormReview: false };
+
+	renderContent() {
+		if (this.state.showFormReview) {
+			return <SurveyFormReview />;
+		}
+
 		return (
-			<>
-				<SurveyForm />
-			</>
+			<SurveyForm
+				onSurveySubmit={() => this.setState({ showFormReview: true })}
+			/>
 		);
+	}
+
+	render() {
+		return <>{this.renderContent()}</>;
 	}
 }
 
