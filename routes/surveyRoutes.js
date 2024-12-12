edc814b1d2d6ce0484a4779c74e42c7a4f3d7771
Colin Parkhurst <parkhurstcolin@protonmail.com>
2024-12-12 10:35:36 -0500

    207

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,8 +10,13 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = (app) => {
+	app.get("/api/surveys", requireLogin, async (req, res) => {
+		const surveys = await Survey.find({ _user: req.user.id });
+	});
+
 	app.get("/api/surveys/:surveyId/:choice", (req, res) => {
 		res.send("Thank you for your input!");
+		res.send(surveys);
 	});
 
 	app.post("/api/surveys/webhooks", (req, res) => {
